b5bf27d65bd4f68107a79aa59c2dd4c5922a14a5
MikeSagnelli <a01229092@itesm.mx>
2018-10-31 13:54:35 -0600

    chore(database): add folders for models for further development

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
       max: 10,
     },
   },
-  test: {
+  develop: {
     client: 'pg',
     connection: {
       host: process.env.RDS_ENDPOINT,
@@ -39,4 +39,24 @@ module.exports = {
       max: 10,
     },
   },
+  // test: { Add test environment with sqlite3
+  //   client: 'pg',
+  //   connection: {
+  //     host: process.env.RDS_ENDPOINT,
+  //     user: process.env.DB_MASTER_USER,
+  //     password: process.env.DB_MASTER_PWD,
+  //     database: process.env.DB_NAME,
+  //   },
+  //   migrations: {
+  //     directory: './app/db/medic/migrations/',
+  //     tableName: 'migrations',
+  //   },
+  //   seeds: {
+  //     directory: './app/db/medic/seeds/',
+  //   },
+  //   pool: {
+  //     min: 0,
+  //     max: 10,
+  //   },
+  // },
 }
